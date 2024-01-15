b83ad35001ba2e6b661ffee8c06561cf483da15c
Alejandro Flores <alejandro.floesc@gmail.com>
2024-01-15 14:02:59 -0600

    Corrección errores3 TipoProducto

--- Springboot/petsupermarket/src/main/java/com/petsupermarket/app/service/TipoProductoService.java
+++ Springboot/petsupermarket/src/main/java/com/petsupermarket/app/service/TipoProductoService.java
@@ -2,19 +2,16 @@ package com.petsupermarket.app.service;
 
 import java.util.List;
 import java.util.Optional;
-
 import com.petsupermarket.app.entity.TipoProducto;
 
 public interface TipoProductoService {
-	
-	TipoProducto getTipoProductoById(Long id);
-	TipoProducto getTipoProductoByTipoProducto(String tipoProducto);
-	static List<TipoProducto> getAllTipoProductos() {
-		return null;
-	}
+
 	TipoProducto getUserByTipoProducto(Long id);
 	TipoProducto crearTipoProducto(TipoProducto tipoProducto);
 	void eliminarTipoProducto(Long id);
 	Optional<TipoProducto> actualizarTipoProducto(Long id, TipoProducto tipoProducto);
+	static List<TipoProducto> getAllTipoProductos() {
+		return null;
+	}
 }
  
