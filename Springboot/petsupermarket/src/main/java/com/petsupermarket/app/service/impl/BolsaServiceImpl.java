29a88fb98c4eee258c09b575a8351b4d12e80e9c
Cynthia Rosales <cynthiarosalesguevara@gmail.com>
2024-01-15 12:14:18 -0600

    bolsa service impl

---
+++ Springboot/petsupermarket/src/main/java/com/petsupermarket/app/service/impl/BolsaServiceImpl.java
@@ -0,0 +1,54 @@
+package com.petsupermarket.app.service.impl;
+
+import com.petsupermarket.app.entity.Bolsa;
+import com.petsupermarket.app.entity.Usuario;
+import com.petsupermarket.app.repository.BolsaRepository;
+import com.petsupermarket.app.service.BolsaService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BolsaServiceImpl implements BolsaService {
+
+    @Autowired
+    private BolsaRepository bolsaRepository;
+
+    @Override
+    public List<Bolsa> obtenerTodasBolsas() {
+        return (List<Bolsa>) bolsaRepository.findAll();
+    }
+
+    @Override
+    public Optional<Bolsa> obtenerBolsaPorId(Long id) {
+        return bolsaRepository.findById(id);
+    }
+
+    @Override
+    public List<Bolsa> obtenerBolsasPorUsuario(Usuario usuario) {
+        return bolsaRepository.findAllByUsuario(usuario);
+    }
+
+    @Override
+    public Bolsa crearBolsa(Bolsa bolsa) {
+        return bolsaRepository.save(bolsa);
+    }
+
+    @Override
+    public Optional<Bolsa> actualizarBolsa(Long id, Bolsa bolsa) {
+        if (bolsaRepository.existsById(id)) {
+            bolsa.setId(id);
+            return Optional.of(bolsaRepository.save(bolsa));
+        }
+        return Optional.empty(); // Manejar la lógica de error
+    }
+
+    @Override
+    public void eliminarBolsa(Long id) {
+        bolsaRepository.deleteById(id);
+    }
+}
